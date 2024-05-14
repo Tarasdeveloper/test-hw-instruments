c959ab0605c6c3c9c5e4b913e6faeda5bdfb78e2
Tarasdeveloper <tomahavki1@gmail.com>
2024-05-14 22:54:23 +0300

    deploy fix

--- src/helpers/findProduct.js
+++ src/helpers/findProduct.js
@@ -1,8 +1,8 @@
-// list.addeEventListener('click', onClick);
+list.addeEventListener('click', onClick);
 
-// function findProduct(elem) {
-//   const productId = Number(elem.closest('.js-card').dataset.id);
-//   return instruments.find(({ id }) => id === productId);
-// }
+function findProduct(elem) {
+  const productId = Number(elem.closest('.js-card').dataset.id);
+  return instruments.find(({ id }) => id === productId);
+}
 
-// export { findProduct };
+export { findProduct };
